94b4e1199eb33e05e477986da89f1aac9716823b
footinglow <footinglow@gmail.com>
2023-03-03 14:15:16 +0900

    キャラクターとアニメーションを作成

---
+++ 02_study/S12_animation/Visual/WalkMan.gd
@@ -0,0 +1,4 @@
+extends Node3D
+
+func play(name):
+	$AnimationPlayer.play(name)
---
+++ 02_study/S12_animation/bullet.gd
@@ -0,0 +1,63 @@
+extends CharacterBody3D
+
+@export var m_v_dir = Vector3.FORWARD
+@export var m_d_speed_mps = 2.0
+
+@export var m_attack_power_ps = 1.0
+@export var m_hp = 1
+
+var m_v3_sensing_pos = null
+var m_v3_enemy_pos = null
+
+func _physics_process(delta):
+	if m_v3_enemy_pos:
+		m_v_dir = m_v3_enemy_pos - transform.origin
+		m_v_dir.y = 0.0
+	else:
+		m_v_dir = Vector3.FORWARD
+	velocity = m_v_dir.normalized() * m_d_speed_mps
+#	print(name, "velocity:", velocity, ",m_v_dir:", m_v_dir, ",m_v_dir.normalized():", m_v_dir.normalized())
+	move_and_slide()
+	
+	# バトル判定
+	var battle_pos = null
+	for index in range(get_slide_collision_count()):
+		var collision = get_slide_collision(index)
+		if (collision.get_collider() == null):
+			continue
+		if collision.get_collider().is_in_group("EnemyGroup"):
+			var enemy = collision.get_collider()
+
+			# バトル中に相手の位置を保持
+			battle_pos = enemy.transform.origin
+
+			# Bulletの攻撃
+			enemy.m_hp -= m_attack_power_ps * delta
+			# Enemyの攻撃
+			m_hp       -= enemy.m_attack_power_ps * delta
+
+	# HPが0になったら消える
+	if m_hp < 0:
+		queue_free()
+	
+	# アニメーションの選択
+	if battle_pos:
+		$WalkMan.play("Battle")
+	else:
+		$WalkMan.play("Walk")
+
+	# バトル中の相手、センシングした敵の順に目標位置を設定する
+	if battle_pos:
+		m_v3_enemy_pos = battle_pos
+	elif m_v3_sensing_pos:
+		if (m_v3_sensing_pos - transform.origin).length() < 0.3:
+			m_v3_sensing_pos = null
+		m_v3_enemy_pos = m_v3_sensing_pos
+	else:
+		m_v3_enemy_pos = null
+	
+func discover_enemy(enemy_pos):
+	m_v3_sensing_pos = enemy_pos
+	
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
