52e3c8f26ac16453f3760c48921e50d98277e7ac
footinglow <footinglow@gmail.com>
2023-03-13 21:40:05 +0900

    【Godot 4.0】スマホ３Dゲームを作るための勉強　その２２　ゲームシステムを作る　その７　PopupPanelをデザインする

---
+++ 02_study/S22_GameSystem_007/settings_popup_panel.gd
@@ -0,0 +1,14 @@
+extends PopupPanel
+
+func open():
+	get_tree().paused = true
+	popup_centered()
+
+func _on_h_slider_drag_ended(value_changed):
+	$ControlBaseSize/VBoxContainer/MarginContainer/Label.text = "発射間隔 %.1f秒" % $ControlBaseSize/VBoxContainer/MarginContainer2/HSlider.value
+
+func _on_button_pressed():
+	hide()
+
+func _on_popup_hide():
+	get_tree().paused = false
