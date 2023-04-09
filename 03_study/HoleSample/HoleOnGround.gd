9b875cf12c62fd9e0c622609a99b729d8663c8b7
footinglow <footinglow@gmail.com>
2023-04-10 00:36:49 +0900

    ArrayMeshに法線を設定して、DirectionalLight3Dの影を有効にしました。

--- 03_study/HoleSample/HoleOnGround.gd
+++ 03_study/HoleSample/HoleOnGround.gd
@@ -19,33 +19,36 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = direction.x
+		velocity.z = direction.z
 
 	# holeの位置を移動
-	m_v3_hole_pos += velocity.normalized()*SPEED*delta
+	m_v3_hole_pos += velocity.normalized() * SPEED * delta
 	set_hole_and_ground_array_mesh()
 	
 # 三角形２つで四角形を作る
 # l:Left, t:Top, r:Right, b:bottom
-func add_square(vertices : PackedVector3Array, x_l, z_t, x_r, z_b):
+func add_square(vertices, normals, x_l, z_t, x_r, z_b):
 	vertices.push_back(Vector3(x_l, 0, z_t))
 	vertices.push_back(Vector3(x_r, 0, z_t))
 	vertices.push_back(Vector3(x_r, 0, z_b))
+	normals.push_back(Vector3.UP)
+	normals.push_back(Vector3.UP)
+	normals.push_back(Vector3.UP)
 
 	vertices.push_back(Vector3(x_r, 0, z_b))
 	vertices.push_back(Vector3(x_l, 0, z_b))
 	vertices.push_back(Vector3(x_l, 0, z_t))
+	normals.push_back(Vector3.UP)
+	normals.push_back(Vector3.UP)
+	normals.push_back(Vector3.UP)
 	
 
 # 落とし穴ArrayMeshを作成する
 func set_hole_and_ground_array_mesh():
 
 	var vertices = PackedVector3Array()
-
+	var normals  = PackedVector3Array()
 	# 地面のXZ平面上の左上、右下の位置を設定
 	var ground_LT = Vector3(-m_d_ground_w_m/2.0 , 0, -m_d_ground_d_m/2.0)
 	var ground_RB = Vector3( m_d_ground_w_m/2.0 , 0,  m_d_ground_d_m/2.0)
@@ -55,17 +58,18 @@ func set_hole_and_ground_array_mesh():
 	var hole_RB = m_v3_hole_pos + hole_size/2.0
 
 	# 3×３グリッドの上３ブロック
-	add_square(vertices, ground_LT.x, ground_LT.z ,ground_RB.x, hole_LT.z)
+	add_square(vertices, normals, ground_LT.x, ground_LT.z ,ground_RB.x, hole_LT.z)
 	# 3×３グリッド、左（holeの左）
-	add_square(vertices, ground_LT.x, hole_LT.z ,hole_LT.x, hole_RB.z)
+	add_square(vertices, normals, ground_LT.x, hole_LT.z ,hole_LT.x, hole_RB.z)
 	# 3×３グリッド、右（holeの右）
-	add_square(vertices, hole_RB.x, hole_LT.z ,ground_RB.x, hole_RB.z)
+	add_square(vertices, normals, hole_RB.x, hole_LT.z ,ground_RB.x, hole_RB.z)
 	# 3×３グリッドの下３ブロック
-	add_square(vertices, ground_LT.x, hole_RB.z ,ground_RB.x, ground_RB.z)
+	add_square(vertices, normals, ground_LT.x, hole_RB.z ,ground_RB.x, ground_RB.z)
 	
 	var arrays = Array()
 	arrays.resize(ArrayMesh.ARRAY_MAX)
 	arrays[ArrayMesh.ARRAY_VERTEX] = vertices
+	arrays[ArrayMesh.ARRAY_NORMAL] = normals
 
 	var tmpMesh = ArrayMesh.new()
 	tmpMesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
