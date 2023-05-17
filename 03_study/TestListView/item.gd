c91a532ac694955dce9e0205bcdd20020c83c207
footinglow <footinglow@gmail.com>
2023-05-17 22:37:27 +0900

    Item表示とsignal接続

---
+++ 03_study/TestListView/item.gd
@@ -0,0 +1,13 @@
+extends Control
+
+signal on_item_button_press(no)
+
+var m_no     : int
+
+func set_item_info(no, name, record):
+	m_no     = no
+	$Panel/Panel2/HBoxContainer/MarginContainer/VBoxContainer/Name.text = name
+	$Panel/Panel2/HBoxContainer/MarginContainer/VBoxContainer/Record.text = record
+
+func _on_button_pressed():
+	on_item_button_press.emit(m_no)
---
+++ 03_study/TestListView/list_view.gd
@@ -0,0 +1,32 @@
+extends Control
+
+@export var m_scn_item : PackedScene
+
+var m_test_list = [
+	["Taro", "Score 90000"],
+	["Jiro", "Score 80000"],
+	["Saburo", "Score 70000"],
+	["Shiro", "Score 60000"],
+	["Goro", "Score 50000"],
+	["Rokuro", "Score 40000"],
+	["Shichiro", "Score 30000"],
+	["Hachiro", "Score 20000"],
+	["Kuro", "Score 10000"],
+	["Juro", "Score 00000"],
+]
+
+
+func _ready():
+	var no=0
+	for info in m_test_list:
+		var ins = m_scn_item.instantiate()
+		ins.set_item_info(no, info[0], info[1])
+		ins.set_custom_minimum_size(Vector2(500, 100))
+		$ScrollContainer/VBoxContainer.add_child(ins)
+		no = no + 1
+		# シグナルを接続する
+		ins.on_item_button_press.connect(on_item_button_press)
+
+
+func on_item_button_press(no):
+	print(no)
