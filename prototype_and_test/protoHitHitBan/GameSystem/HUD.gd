e30828c8dca441862815205d2a1e137291cf8d25
footinglow <footinglow@gmail.com>
2023-11-03 13:09:09 +0900

    プライヤーの位置は、ドラッグした分移動するように修正

--- prototype_and_test/protoHitHitBan/GameSystem/HUD.gd
+++ prototype_and_test/protoHitHitBan/GameSystem/HUD.gd
@@ -10,16 +10,22 @@ func _process(delta):
 
 func _draw():
 	# ドラッグ量の可視化
-	if f_diplay_drag_pos:
-		# UI表示
-		var circle_pos = m_v2_drag_pos
-		if (m_v2_drag_pos-m_v2_touch_pos).length() >= g_c_.m_d_ui_max_px_len:
-			var rad = atan2(m_v2_drag_pos.y - m_v2_touch_pos.y, m_v2_drag_pos.x - m_v2_touch_pos.x)
-			circle_pos = m_v2_touch_pos + Vector2(cos(rad), sin(rad)) * g_c_.m_d_ui_max_px_len
-		draw_circle(m_v2_touch_pos, g_c_.m_d_ui_max_px_len + g_c_.m_d_design_ctrl_circle_r, Color(1, 1, 1, 0.2))
-		draw_circle(circle_pos,   g_c_.m_d_design_ctrl_circle_r, Color(1, 1, 1, 0.5))
+	if g_c_.m_ui_kind == g_c_.UI_KIND.TOUCH_PAD:	
+		if f_diplay_drag_pos:
+			# UI表示
+			var circle_pos = m_v2_drag_pos
+			if (m_v2_drag_pos-m_v2_touch_pos).length() >= g_c_.m_d_ui_max_px_len:
+				var rad = atan2(m_v2_drag_pos.y - m_v2_touch_pos.y, m_v2_drag_pos.x - m_v2_touch_pos.x)
+				circle_pos = m_v2_touch_pos + Vector2(cos(rad), sin(rad)) * g_c_.m_d_ui_max_px_len
+			draw_circle(m_v2_touch_pos, g_c_.m_d_ui_max_px_len + g_c_.m_d_design_ctrl_circle_r, Color(1, 1, 1, 0.2))
+			draw_circle(circle_pos,   g_c_.m_d_design_ctrl_circle_r, Color(1, 1, 1, 0.5))
 
-		f_diplay_drag_pos = false
+			f_diplay_drag_pos = false
+		else:
+			pass	# タッチしていないときは更新しない
+	else:
+		# タッチパッド以外は表示不要
+		pass	
 			
 
 func _on_game_sig_to_hud_drag_line(touch_pos, drag_pos):
--- prototype_and_test/protoHitHitBan/GameSystem/game.gd
+++ prototype_and_test/protoHitHitBan/GameSystem/game.gd
@@ -9,10 +9,14 @@ signal sig_to_hud_drag_line(touch_pos, drag_pos)
 
 var m_firing_remain_time_sec = 0
 
-# ドラッグ操作で床を傾ける
+# ドラッグ操作検知用
 var m_touch_pos = Vector2.ZERO		# タッチした時の位置
 var m_drag_pos = Vector2.ZERO		# 現在のタッチ位置・ドラッグ位置
 var m_f_is_screen_touch = false		# スクリーンにタッチ（ドラッグ含む）している場合true
+
+# DRAG_TO_MOVEドラッグした分移動する操作方法）の時用
+var m_v_base_player_pos = Vector3.ZERO
+
 
 func _input(event):
 	if event is InputEventScreenTouch:
@@ -21,14 +25,20 @@ func _input(event):
 			m_touch_pos  = event.position
 			m_drag_pos = event.position
 			m_f_is_screen_touch = true
+			# m_touch_posを設定したときのプレイヤーの位置を基準として記憶する
+			m_v_base_player_pos = $player.global_position
 		else:
+			# 指を離したのを検知する
 			m_f_is_screen_touch = false
 	elif event is InputEventScreenDrag:
 		if m_touch_pos == Vector2.ZERO:
 			# READY状態にタッチするとWorldが動き出すため最初のタッチが検知されないためここで初期値を設定する
 			m_touch_pos = event.position
+			# m_touch_posを設定したときのプレイヤーの位置を基準として記憶する
+			m_v_base_player_pos = $player.global_position
 		m_drag_pos = event.position
 		m_f_is_screen_touch = true
+
 
 func _physics_process(delta):
 	if m_f_is_screen_touch:
@@ -39,12 +49,18 @@ func _physics_process(delta):
 			var diff_y_px = m_drag_pos.y - g_c_.m_v2_ui_center_pos.y
 			# 移動方向を設定
 			v_dir = Vector3(diff_x_px, 0, diff_y_px).normalized()
+			# Playerの移動	
+			$player.velocity = v_dir * m_speed_mps
+			$player.move_and_slide()
+		elif g_c_.m_ui_kind == g_c_.UI_KIND.DRAG_TO_MOVE:
+			var camera = get_viewport().get_camera_3d()
+			var v_touch_pos = camera.project_position(m_touch_pos, camera.global_position.y)
+			var v_drag_pos  = camera.project_position(m_drag_pos,  camera.global_position.y)
+			v_touch_pos.y = 0
+			v_drag_pos.y = 0
+			$player.global_position = m_v_base_player_pos + ( v_drag_pos - v_touch_pos )
 		else:
 			pass
-
-		# Playerの移動	
-		$player.velocity = v_dir * m_speed_mps
-		$player.move_and_slide()
 		
 		# タイミングをとりながらlaser発射
 		if m_firing_remain_time_sec > 0:
